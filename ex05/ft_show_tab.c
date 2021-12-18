6937e3bcbd5c1b12cb19f6e8713686df54222a0d
fleizean <nsyaqz@gmail.com>
2021-12-18 13:01:45 +0300

    42-Piscine git commit -m 42-Piscine git commit -m 42-Piscine

---
+++ ex05/ft_show_tab.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eyagiz <eyagiz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 15:49:02 by eyagiz            #+#    #+#             */
+/*   Updated: 2021/12/15 15:49:06 by eyagiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putstr("\n");
+		ft_putnbr(par[i].size);
+		ft_putstr("\n");
+		ft_putstr(par[i].copy);
+		ft_putstr("\n");
+		i++;
+	}
+}
